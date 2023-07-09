fb8127bdb3c656ae81b5363ae59035ae42cc463c
khurshed <hurshed@gmail.com>
2023-07-09 19:03:01 +0500

    Добавил решение задачи используя функцию Мах и элементов индекса массива

--- Example009_IntroArrey/Program.cs
+++ Example009_IntroArrey/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное число. Пример с использованием функции и массива.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+
+    return result;
+} //            0    1   2   3   4   5   6   7   8 Элемент индекса массива
+int[] arrey = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+
+int result = Max(
+    Max(arrey[0], arrey[1], arrey[2]),
+    Max(arrey[3], arrey[4], arrey[5]),
+    Max(arrey[6], arrey[7], arrey[8]));
+Console.WriteLine(result);
+
