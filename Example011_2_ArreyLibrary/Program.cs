52427795632d3d4a22441864f708dab883897e8f
khurshed <hurshed@gmail.com>
2023-07-09 20:44:46 +0500

    Добавил папку 011_2

---
+++ Example011_2_ArreyLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArrey(int[] collection) // если метод ничего не возвращает, вот он называется vpid  методом
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArrey(int[] col)
+{
+
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] arrey = new int[10]; //конструкция new int[10]; означает дословно "Создай новый массив в котором будет 10 элементов" 
+
+FillArrey(arrey);
+PrintArrey(arrey);
