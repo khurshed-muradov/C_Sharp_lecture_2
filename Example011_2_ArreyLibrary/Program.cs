b3767332e7283fd8e9c630306f8f54a01308f740
khurshed <hurshed@gmail.com>
2023-07-09 21:23:09 +0500

    Добавил решение задачи 2

--- Example011_2_ArreyLibrary/Program.cs
+++ Example011_2_ArreyLibrary/Program.cs
@@ -23,7 +23,30 @@ void PrintArrey(int[] col)
 
 }
 
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] arrey = new int[10]; //конструкция new int[10]; означает дословно "Создай новый массив в котором будет 10 элементов" 
 
 FillArrey(arrey);
+arrey[4] = 4;
+arrey[6] = 4;
+
 PrintArrey(arrey);
+Console.WriteLine();
+int pos = indexOf(arrey, 444);
+Console.WriteLine(pos);
