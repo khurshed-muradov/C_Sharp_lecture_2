9a071bd32612e7e8daf807a32cc6e99a786e412e
khurshed <hurshed@gmail.com>
2023-07-09 13:37:42 +0500

    Добавил 2 решения для определения максимального числа используя функции

--- Example008_2_IntroMethod/Program.cs
+++ Example008_2_IntroMethod/Program.cs
@@ -1,2 +1,31 @@
 ﻿// Найти максимальное число. Пример с использованием функции.
 
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+
+    return result;
+}
+int a1 = 151231;
+int b1 = 21;
+int c1 = 39;
+
+int a2 = 12;
+int b2 = 2311;
+int c2 = 33;
+
+int a3 = 13;
+int b3 = 23;
+int c3 = 313;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); //Второй вариант определения максимального числа из трех отдельных аргументов.
+
+Console.WriteLine(max);
+
