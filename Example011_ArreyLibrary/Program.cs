9f934de955a23b51ece0ac06f42f5cf2ad597298
khurshed <hurshed@gmail.com>
2023-07-09 20:42:27 +0500

    Добавлено решение задачи

--- Example011_ArreyLibrary/Program.cs
+++ Example011_ArreyLibrary/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArrey(int[] collection) // если метод ничего не возвращает, вот он называется vpid  методом
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArrey(int[] col)
+{
+
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] arrey = new int[10]; //конструкция new int[10]; означает дословно "Создай новый массив в котором будет 10 элементов" 
+
+FillArrey(arrey);
+PrintArrey(arrey);
